f1cf0363e6f8bdeb6d9d466f56f0f8a830d32a3f
dotnetbysomik <dotnetbysomik@gmail.com>
2026-04-28 02:06:41 +0300

    Added test changes

--- aspnet-task-api/Data/AppDbContext.cs
+++ aspnet-task-api/Data/AppDbContext.cs
@@ -8,6 +8,6 @@ namespace aspnet_task_api.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options) { }
 
-        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();;
     }
 }
--- aspnet-task-api/Models/Task.cs
+++ aspnet-task-api/Models/Task.cs
@@ -9,4 +9,14 @@
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? DueDate { get; set; }
     }
+
+        public class TaskItem2
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DueDate { get; set; }
+    }
 }
